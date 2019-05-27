4acddcfeaf215c5f27f3dfd1438d96199cd62bea
fabiocax <fabiocax@gmail.com>
2019-05-27 10:05:35 -0300

    Has

---
+++ EncontrarFaces.py
@@ -0,0 +1,33 @@
+import cv2
+import sys
+
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.04,
+    minNeighbors=4,
+    minSize=(1, 1)
+)
+
+print("Opa, {0} faces encontradas!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 255), 2)
+
+cv2.imshow("Faces encontradas", image)
+
+
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    sys.exit()
